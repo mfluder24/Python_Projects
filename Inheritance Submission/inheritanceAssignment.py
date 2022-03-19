7ae4170462c63505066284d9945ceee2954bb186
Fox <99509225+mfluder24@users.noreply.github.com>
2022-03-19 16:54:04 +1300

    Added Inheritance Assignment folder and contents
    added inheritanceAssignment.py

---
+++ Inheritance Submission/inheritanceAssignment.py
@@ -0,0 +1,17 @@
+class Animal:
+    # Define attributes of the the parent class
+    name = ""
+    species = ""
+    diet = ""
+    age = 0
+
+class Crow(Animal):
+    # Define attributes of the the child class
+    flySpeed = 42
+    canSpeak = True
+
+class RainbowTrout(Animal):
+    # Define attributes of the the child class
+    swimSpeed = 4
+    finRot = False
+    
